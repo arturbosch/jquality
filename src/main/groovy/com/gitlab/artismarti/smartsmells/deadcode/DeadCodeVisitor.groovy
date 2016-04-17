78b04d5677b86a796db7aeffd72c2aa7bf593de9
Arti Smarti <arturbosch@gmx.de>
2016-04-17 22:33:19 +0200

    Build map of member names to counter

--- src/main/groovy/com/gitlab/artismarti/smartsmells/deadcode/DeadCodeVisitor.groovy
+++ src/main/groovy/com/gitlab/artismarti/smartsmells/deadcode/DeadCodeVisitor.groovy
@@ -5,6 +5,8 @@ import com.gitlab.artismarti.smartsmells.common.NodeHelper
 import com.gitlab.artismarti.smartsmells.common.Visitor
 
 import java.nio.file.Path
+import java.util.function.Function
+import java.util.stream.Collectors
 
 /**
  * @author artur
@@ -13,8 +15,8 @@ class DeadCodeVisitor extends Visitor<DeadCode> {
 
 	private boolean onlyPrivate
 
-	List<String> methods
-	List<String> fields
+	private Map<String, Integer> methods
+	private Map<String, Integer> fields
 
 	DeadCodeVisitor(Path path, boolean onlyPrivate) {
 		super(path)
@@ -24,7 +26,12 @@ class DeadCodeVisitor extends Visitor<DeadCode> {
 	@Override
 	void visit(CompilationUnit n, Object arg) {
 		methods = NodeHelper.findPrivateMethods(n)
+				.stream()
+				.collect(Collectors.toMap(Function.identity(), { method -> 0 }))
 		fields = NodeHelper.findPrivateFields(n)
+				.stream()
+				.collect(Collectors.toMap(Function.identity(), { method -> 0 }))
+
 		super.visit(n, arg)
 	}
 }
