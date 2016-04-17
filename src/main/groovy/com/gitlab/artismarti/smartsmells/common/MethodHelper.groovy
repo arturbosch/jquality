d59aafcda9807a5a1e398538b28422ae4da7122e
Arti Smarti <arturbosch@gmx.de>
2016-04-17 21:23:27 +0200

    Extract is getter or setter helper method to MethodHelper

--- src/main/groovy/com/gitlab/artismarti/smartsmells/common/MethodHelper.groovy
+++ src/main/groovy/com/gitlab/artismarti/smartsmells/common/MethodHelper.groovy
@@ -5,6 +5,10 @@ import com.github.javaparser.ast.body.BodyDeclaration
 import com.github.javaparser.ast.body.ConstructorDeclaration
 import com.github.javaparser.ast.body.MethodDeclaration
 import com.github.javaparser.ast.body.Parameter
+import com.github.javaparser.ast.expr.AssignExpr
+import com.github.javaparser.ast.expr.NameExpr
+import com.github.javaparser.ast.stmt.ExpressionStmt
+import com.github.javaparser.ast.stmt.ReturnStmt
 import com.github.javaparser.ast.stmt.Statement
 import com.gitlab.artismarti.smartsmells.complexmethod.CyclomaticComplexityVisitor
 
@@ -32,4 +36,31 @@ class MethodHelper {
 				n.parameters : ((MethodDeclaration) n).parameters
 	}
 
+	static boolean isGetterOrSetter(List<Statement> stmts) {
+		if (stmts.size() == 1) {
+			def statement = stmts.get(0)
+			return isGetter(statement) || isSetter(statement)
+		}
+		return false
+	}
+
+	static boolean isSetter(Statement statement) {
+		if (statement instanceof ExpressionStmt) {
+			def expression = ((ExpressionStmt) statement).expression
+			if (expression instanceof AssignExpr) {
+				return true
+			}
+		}
+		return false
+	}
+
+	static boolean isGetter(Statement statement) {
+		if (statement instanceof ReturnStmt) {
+			def expression = ((ReturnStmt) statement).expr
+			if (expression instanceof NameExpr) {
+				return true
+			}
+		}
+		return false
+	}
 }
