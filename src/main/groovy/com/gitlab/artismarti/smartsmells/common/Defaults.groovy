a3240288b9b373fba44da1c527cc4e6e21ede6db
Arti Smarti <arturbosch@gmx.de>
2016-04-24 22:15:49 +0200

    Change defaults values from def to corresponding type

--- src/main/groovy/com/gitlab/artismarti/smartsmells/common/Defaults.groovy
+++ src/main/groovy/com/gitlab/artismarti/smartsmells/common/Defaults.groovy
@@ -5,14 +5,15 @@ package com.gitlab.artismarti.smartsmells.common
  */
 class Defaults {
 
-	static def LONG_METHOD = 7
-	static def LONG_PARAMETER_LIST = 5
-	static def COMPLEX_METHOD = 10
-	static def WEIGHTED_METHOD_COUNT = 20
-	static def ACCESS_TO_FOREIGN_DATA = 4
-	static def TIED_CLASS_COHESION = 0.33
-	static def ONLY_PRIVATE_DEAD_CODE = true
-	static def LARGE_CLASS = 150
-	static def CHAIN_SIZE = 2
+	static int LONG_METHOD = 7
+	static int LONG_PARAMETER_LIST = 5
+	static int COMPLEX_METHOD = 10
+	static int  WEIGHTED_METHOD_COUNT = 20
+	static int ACCESS_TO_FOREIGN_DATA = 4
+	static double TIED_CLASS_COHESION = 0.33
+	static boolean ONLY_PRIVATE_DEAD_CODE = true
+	static int LARGE_CLASS = 150
+	static int CHAIN_SIZE = 2
+	static double FEATURE_ENVY_FACTOR = 0.5
 
 }
