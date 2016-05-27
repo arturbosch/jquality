cb07f6fd558a88795c917310e8392d09d9128763
Arti Smarti <arturbosch@gmx.de>
2016-05-27 15:04:32 +0200

    Add a test case if a method is long, complex and has many parameters

---
+++ src/test/groovy/com/gitlab/artismarti/smartsmells/integration/IsLongIsComplexHasManyParamsMethodIT.groovy
@@ -0,0 +1,36 @@
+package com.gitlab.artismarti.smartsmells.integration
+
+import com.gitlab.artismarti.smartsmells.complexmethod.ComplexMethod
+import com.gitlab.artismarti.smartsmells.config.Smell
+import com.gitlab.artismarti.smartsmells.longmethod.LongMethod
+import com.gitlab.artismarti.smartsmells.longparam.LongParameterList
+import com.gitlab.artismarti.smartsmells.start.DetectorFacade
+import spock.lang.Specification
+
+import java.nio.file.Paths
+
+/**
+ * @author artur
+ */
+class IsLongIsComplexHasManyParamsMethodIT extends Specification {
+
+	def "complex, long and long parameter methods have same signature"() {
+		given:
+		def path = Paths.get(getClass().getResource("/invalid/GodClass.java").getFile())
+
+		when:
+		def result = DetectorFacade.builder().fullStackFacade().run(path)
+		def lm = ((LongMethod) result.of(Smell.LONG_METHOD).getFirst()).signature
+		def cm = ((ComplexMethod) result.of(Smell.COMPLEX_METHOD).getFirst()).signature
+		def lpl = ((LongParameterList) result.of(Smell.LONG_PARAM).getFirst()).signature
+
+		then:
+		result.of(Smell.COMPLEX_METHOD).size() == 1
+		result.of(Smell.LONG_METHOD).size() == 1
+		result.of(Smell.LONG_PARAM).size() == 1
+		lm == cm
+		cm == lpl
+		lm == lpl
+
+	}
+}
---
+++ src/test/resources/invalid/GodClass.java
@@ -0,0 +1,49 @@
+public class Godclass {
+
+	public void bigMethod(final String vorname, final String nachname, final int alter, final Date bday,
+	                      final String hobbies, final int zahl1, final int zahl2, final int zahl3, final int zahl4,
+	                      final int zahl5, final int zahl6) {
+
+		String vollerName = "";
+		if (!vorname.isEmpty()) {
+			if (!nachname.isEmpty()) {
+				vollerName = vorname + " " + nachname;
+			}
+		}
+
+		if (!vorname.isEmpty()) {
+			if (!nachname.isEmpty()) {
+				vollerName = vorname + " " + nachname;
+			}
+		}
+
+		if (!vorname.isEmpty()) {
+			if (!nachname.isEmpty()) {
+				vollerName = vorname + " " + nachname;
+			}
+		}
+
+		if (!vorname.isEmpty()) {
+			if (!nachname.isEmpty()) {
+				vollerName = vorname + " " + nachname;
+			}
+		}
+
+		String nameAlter = "";
+		if (!vollerName.isEmpty()) {
+			nameAlter = vollerName + ", " + alter;
+		}
+
+		final DateFormat richtigerBday = new SimpleDateFormat("HH:mm:ss");
+
+		String mehrDaten = "";
+		if (!mehrDaten.isEmpty()) {
+			mehrDaten = nameAlter + ", " + richtigerBday.format(bday);
+		}
+
+		String nochMehrDaten = "";
+		if (!nochMehrDaten.isEmpty()) {
+			nochMehrDaten = mehrDaten + ", \n" + hobbies;
+		}
+	}
+}
