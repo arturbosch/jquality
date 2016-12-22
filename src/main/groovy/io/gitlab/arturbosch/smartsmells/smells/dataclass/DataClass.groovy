4057993dca253f36e4bd18ba1bb400498dcd4e11
abosch <arturbosch@gmx.de>
2016-12-22 22:02:26 +0100

    Exclude package info from DataClass toString method

--- src/main/groovy/io/gitlab/arturbosch/smartsmells/smells/dataclass/DataClass.groovy
+++ src/main/groovy/io/gitlab/arturbosch/smartsmells/smells/dataclass/DataClass.groovy
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.smartsmells.smells.dataclass
 
 import groovy.transform.Immutable
+import groovy.transform.ToString
 import io.gitlab.arturbosch.jpal.ast.source.SourcePath
 import io.gitlab.arturbosch.jpal.ast.source.SourceRange
 import io.gitlab.arturbosch.smartsmells.common.DetectionResult
@@ -9,6 +10,7 @@ import io.gitlab.arturbosch.smartsmells.common.DetectionResult
  * @author artur
  */
 @Immutable
+@ToString(includePackage = false)
 class DataClass implements DetectionResult {
 
 	String name
