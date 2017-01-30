65a0430b22937f2af54cb1831eac6641d0a606e8
abosch <arturbosch@gmx.de>
2017-01-30 21:59:12 +0100

    Add test case that interfaces are not middle man's - Closes#82

--- src/test/groovy/io/gitlab/arturbosch/smartsmells/java/MiddleManDummy.java
+++ src/test/groovy/io/gitlab/arturbosch/smartsmells/java/MiddleManDummy.java
@@ -28,6 +28,27 @@ public class MiddleManDummy {
 		}
 	}
 
+	/**
+	 * Interfaces with default methods look like Middle Man's but are only specifications
+	 * with some sugar for simple cases or as substitute for traits as example given here.
+	 * We will not treat them as MM because interfaces only do not justify the definition of MM.
+	 */
+	private interface InterfaceInTheMiddle {
+		EndMan endMan();
+
+		default String middleLogic(String s) {
+			return endMan().realLogic(s);
+		}
+
+		default String middleLogic2(String s) {
+			return endMan().realLogic(s);
+		}
+
+		default String middleLogic3(String s) {
+			return endMan().realLogic(s);
+		}
+	}
+
 	private class EndMan {
 		String realLogic(String s) {
 			return s;
