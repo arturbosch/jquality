249800d3ff43eff0af5a21dc6ae56201c62deec6
abosch <arturbosch@gmx.de>
2017-04-14 21:46:27 +0200

    Extend metric raisers test case - #101 #52

--- src/test/groovy/io/gitlab/arturbosch/smartsmells/metrics/internal/MetricRaiserTest.groovy
+++ src/test/groovy/io/gitlab/arturbosch/smartsmells/metrics/internal/MetricRaiserTest.groovy
@@ -9,23 +9,26 @@ import spock.lang.Specification
  */
 class MetricRaiserTest extends Specification {
 
-	def "raise metrics for dummies"() {
-		given: "composite metric raiser for all supported metrics"
+	def "raise metrics for godclass dummy"() {
+		given: "composite metric raiser for all supported godClass"
 		def lm = new LongMethodAverageAndDeviation()
 		def lpl = new LongParameterListAverageAndDeviation()
 		def wmc = new WMC()
 		def atfd = new ATFD()
+		def tcc = new TCC()
 		def mcc = new MCCabe()
 		def loc = new LOC()
 		def sloc = new SLOC()
-		def composite = new SimpleCompositeMetricRaiser([wmc, atfd, mcc, loc, sloc])
+		def noa = new NOA()
+		def nom = new NOM()
+		def composite = new SimpleCompositeMetricRaiser([wmc, atfd, tcc, mcc, loc, sloc, nom, noa])
 		def metricRaiser = new CombinedCompositeMetricRaiser([lm, lpl, composite])
-		when: "querying all classes"
-		def classes = Test.compile(Test.GOD_CLASS_DUMMY_PATH)
-				.getNodesByType(ClassOrInterfaceDeclaration.class)
-		classes.collect { metricRaiser.raise(it) }.each { println it }
-		then:
-		true
+		when: "querying all godClass"
+		def godClass = Test.compile(Test.GOD_CLASS_DUMMY_PATH)
+				.getNodesByType(ClassOrInterfaceDeclaration.class)[0]
+		def metrics = metricRaiser.raise(godClass)
+		then: "8 godClass + 2 composite godClass (4) = 12"
+		metrics.size() == 12
 	}
 
 }
