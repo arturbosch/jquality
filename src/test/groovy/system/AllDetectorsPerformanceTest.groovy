9586930826f7fc62fefabab82e5b87d2ebc02b81
Artur Bosch <arturbosch@gmx.de>
2017-12-12 14:40:30 +0100

    Run performance tests for all detectors

--- src/test/groovy/system/AllDetectorsPerformanceTest.groovy
+++ src/test/groovy/system/AllDetectorsPerformanceTest.groovy
@@ -5,6 +5,7 @@ import io.gitlab.arturbosch.jpal.core.JPAL
 import io.gitlab.arturbosch.jpal.resolution.Resolver
 import io.gitlab.arturbosch.smartsmells.Main
 import io.gitlab.arturbosch.smartsmells.api.DetectorFacade
+import io.gitlab.arturbosch.smartsmells.metrics.FileMetricProcessor
 import spock.lang.Specification
 
 import java.nio.file.Paths
@@ -16,12 +17,13 @@ class AllDetectorsPerformanceTest extends Specification {
 
 	def "all detectors"() {
 		given:
-		def path = "/home/artur/Repos/elasticsearch/core/src/main/"
+		def path = Paths.get("/home/artur/master/projects/elasticsearch/core/src/main/java")
 		CompilationStorage storage
 		def time = Main.benchmark {
-			storage = JPAL.newInstance(Paths.get(path))
+			storage = JPAL.newInstance(path, new FileMetricProcessor())
 		} // ~7500 to ~8500 ms
 		// JPAL RC5: ~9600 ms
+		// Metric Preprocessing: 21823
 		println "CompilationStorage: $time ms"
 		def facade = DetectorFacade.builder().fullStackFacade()
 
@@ -35,6 +37,7 @@ class AllDetectorsPerformanceTest extends Specification {
 		} //  27574 (w/o SS) ms, 35351 (w/ SS) ms
 		// JPAL RC2: 23k-24456 ms, 33k ms
 		// JPAL RC5: 34k ms
+		// Metric Preprocessing: 14110
 		println "Detectors: $time ms"
 		println "#detectors: $detectors"
 		println "#smells: $smells"
