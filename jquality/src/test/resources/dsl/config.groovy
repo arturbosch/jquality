b4d883d782a612546e24f65159c1f9a01672747b
Artur Bosch <arturbosch@gmx.de>
2017-12-16 16:51:21 +0100

    Unignore test resources

---
+++ jquality/src/test/resources/dsl/config.groovy
@@ -0,0 +1,24 @@
+package dsl
+//noinspection GroovyAssignabilityCheck
+config {
+
+	input "/path/to/project"
+	output "/path/to/output"
+
+	filters {
+		filter ".*/test/.*"
+	}
+
+	metrics(['wmc', 'atfd', 'tcc'])
+	jars(['/home/artur/Repos/SmartSmells/src/test/resources/detector.jar'])
+
+	// do not change amount of detectors! see DetectorFacadeBuilderIntegrationTest
+	// may be extended for DetectorConfigDslRunnerTest
+	detectors {
+		detector("longmethod") {
+			let('active', 'true')
+			let('threshold', '20')
+		}
+	}
+
+}
